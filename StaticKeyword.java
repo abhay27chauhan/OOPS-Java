83c8f4c4c5f70b3ce5537bbd3464a9a71cbee38e
Abhay <chauhan27.abhay@gmail.com>
2021-01-03 10:59:37 +0530

    Static Keyword added few more points

--- StaticKeyword.java
+++ StaticKeyword.java
@@ -2,19 +2,44 @@ public class StaticKeyword{ // refer point 6.
 
     static int a = 2; // refers to point 7
 
+    int b = 3;
+
     class Person{
         int age;
         String name;
 
-        /* static String breed = "Homosapiens"; -> throw an error refer to 
+        /* static String breed = "Homosapiens"; -> throw an error -> refer to 
                                                    point 7 */
     }
 
-    static class C{  // refer to point 6
+    static class Test{  // refer to point 6
 
     }
 
-    public static void main(String[] args){} // refer to point 7
+    public static void main(String[] args){ // refer to point 7
+
+        Person p = new Person(); // error: non-static variable this cannot be referenced from a static context
+
+        b = 5; // error: non-static variable a cannot be referenced from a static context (refer to point 8)
+
+        Test t = new Test();
+
+        A objA = new A();
+        A.B objB = objA.new B();
+
+        A.C objC = new A.C();
+    }
+}
+
+class A{
+
+    class B{
+       
+    }
+
+    static class C{
+        
+    }
 }
 
 
@@ -33,5 +58,8 @@ public class StaticKeyword{ // refer point 6.
 
     7. we can use 'static' keyword inside main class (top-level class) but cannot use 'static' keyword inside a non-static inner (or nested) class.
 
-    8. we also cannot used non-static (variable | methods | class (make objects in case of class)) inside a 'static' method.
+    8. we also cannot use non-static (variable | methods | class (cannot make object in case of class)) inside a 'static' method and also cannot make object of non-static class inside a static class;
+
+    9. benefit of created static nested class - we can access that class as if it is outside of that parent class
+    for eg -> In above case if i want of make the object of Person class, first i have to make the object of StaticKeyword class then only i can make the object of Person. But if i want to make the object of Test class, i can do it directly.
  */
